e00c23126fda818b3695509abc579579952fcf1b
mathieu cans <mathieucans@gmail.com>
2021-08-27 15:32:01 +0200

    Extract simple mail server class

--- simplemail/src/SimpleMailServer.spec.ts
+++ simplemail/src/SimpleMailServer.spec.ts
@@ -1,26 +1,15 @@
-import http from 'http';
-import express, {json} from 'express'
 import request from 'supertest'
+import {SimpleMailServer} from "./SimpleMailServer";
 
 describe('simple mail server', () => {
     test('send a mail with success', async () => {
-        const app = express();
-        app.use(json());
-        app.post('/sendMail', (req, resp) => {
-            resp.send(200);
-        });
-        let server: http.Server | undefined = undefined;
-        await new Promise((resolve) => {
-            server = app?.listen(80, () => {
-                resolve(0);
-            });
-        });
+        const mailServer = new SimpleMailServer();
+        await mailServer.start();
 
-        await request(app).post('/sendMail')
+        await request('http://localhost:80').post('/sendMail')
             .expect(200)
 
-        await new Promise((resolve) => {
-            server?.close(resolve)
-        });
+        await mailServer.stop();
+
     });
 });
---
+++ simplemail/src/SimpleMailServer.ts
@@ -0,0 +1,27 @@
+import http from "http";
+import express, {json} from "express";
+
+export class SimpleMailServer {
+    private server?: http.Server;
+
+    start() {
+        const app = express();
+        app.use(json());
+        app.post('/sendMail', (req, resp) => {
+            resp.sendStatus(200);
+        });
+
+        return new Promise((resolve) => {
+            this.server = app?.listen(80, () => {
+                resolve(0);
+            });
+        });
+
+    }
+
+    async stop() {
+        return await new Promise((resolve) => {
+            this.server?.close(resolve)
+        });
+    }
+}
