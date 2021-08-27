b0abe421f9dad353e70f7dedb5bb32381acd335b
mathieu cans <mathieucans@gmail.com>
2021-08-27 15:42:27 +0200

    move server start and stop in before and after each sections

--- simplemail/src/SimpleMailServer.spec.ts
+++ simplemail/src/SimpleMailServer.spec.ts
@@ -2,14 +2,19 @@ import request from 'supertest'
 import {SimpleMailServer} from "./SimpleMailServer";
 
 describe('simple mail server', () => {
-    test('send a mail with success', async () => {
-        const mailServer = new SimpleMailServer();
+    let mailServer: SimpleMailServer;
+    beforeEach(async () => {
+        mailServer = new SimpleMailServer();
         await mailServer.start();
+    });
 
+    afterEach(async () => {
+        await mailServer.stop();
+    });
+
+    test('send a mail with success', async () => {
         await request('http://localhost:80').post('/sendmail')
             .expect(200)
-
-        await mailServer.stop();
 
     });
 });
