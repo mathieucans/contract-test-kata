1f0393eefe41a772641eeec9742355880748edcf
mathieu cans <mathieucans@gmail.com>
2021-09-02 09:33:14 +0200

    remove duplication in simple mail server tests

--- simplemail/src/SimpleMailServer.spec.ts
+++ simplemail/src/SimpleMailServer.spec.ts
@@ -3,18 +3,25 @@ import {SimpleMailServer} from "./SimpleMailServer";
 import {MailBox} from "./MailBox";
 import {Mail} from "./Mail";
 
+
+const douglasMail = 'douglas.hofstadter@simplemail.com';
+const billsMail = 'billy.thekid@simplemail.com';
+const jordanMail = 'magic.jordan@simplemail.com';
+const douglasId = 1;
+const billsId = 2;
 const billysMessages = [
-    new Mail('douglas.hofstadter@simplemail.com', 'billy.thekid@simplemail.com', 'Hello from vienne', 'Hi bill, how are you?'),
-    new Mail('magic.jordan@simplemail.com', 'billy.thekid@simplemail.com', 'Hello from LA', 'Hi bill, how are you?'),
-]
+    new Mail(douglasMail, billsMail, 'Hello from vienne', 'Hi bill, how are you?'),
+    new Mail(jordanMail, billsMail, 'Hello from LA', 'Hi bill, how are you?'),
+];
 
 describe('simple mail server', () => {
+    const baseUrl = 'http://localhost:80';
     let mailServer: SimpleMailServer;
     beforeEach(async () => {
         mailServer = new SimpleMailServer([
-            new MailBox(1, 'douglas.hofstadter@simplemail.com'),
-            new MailBox(2, 'billy.thekid@simplemail.com', billysMessages),
-            new MailBox(3, 'magic.jordan@simplemail.com')]);
+            new MailBox(douglasId, douglasMail),
+            new MailBox(billsId, billsMail, billysMessages),
+            new MailBox(3, jordanMail)]);
         await mailServer.start();
     });
 
@@ -24,33 +31,32 @@ describe('simple mail server', () => {
 
     test('list received mails of a user', async () => {
         let jsonBillsMessages = billysMessages.map(m => JSON.parse(JSON.stringify(m)));
-        await request('http://localhost:80')
-            .get('/2/mails')
+        await request(baseUrl)
+            .get(`/${billsId}/mails`)
             .expect(200, {
-                mails : jsonBillsMessages
+                mails: jsonBillsMessages
             });
     });
 
     test('send a mail with success', async () => {
-        await request('http://localhost:80')
-            .post('/1/mails/send')
+        await request(baseUrl)
+            .post(`/${douglasId}/mails/send`)
             .expect(200)
     });
 
-
     test('cannot send a mail with wrong user', async () => {
-        await request('http://localhost:80')
+        await request(baseUrl)
             .post('/12354/mails/send')
             .expect(404)
     });
 
     test('list users', async () => {
-        await request('http://localhost:80').get('/users')
+        await request(baseUrl).get('/users')
             .expect(200, {
                 users: [
-                    {id:1, email:'douglas.hofstadter@simplemail.com'},
-                    {id:2, email:'billy.thekid@simplemail.com'},
-                    {id:3, email:'magic.jordan@simplemail.com'},
+                    {id: douglasId, email: douglasMail},
+                    {id: billsId, email: billsMail},
+                    {id: 3, email: jordanMail},
                 ]
             });
     });
