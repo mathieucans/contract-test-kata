1b35ed85d714e9b70051b4a0c6982a75110e987e
mathieu cans <mathieucans@gmail.com>
2021-09-09 14:03:14 +0200

    Clean up test for demo

--- simplemail-client/typescript/src/simplemail.integration.spec.ts
+++ simplemail-client/typescript/src/simplemail.integration.spec.ts
@@ -1,34 +1,5 @@
-import {SimpleMailApi} from "./simpleMailApi";
-
-
 describe('Simple mail api integration tests', () => {
-    let simpleMail: SimpleMailApi;
-    beforeEach(() => {
-        simpleMail = new SimpleMailApi();
-    });
-
-    test('retrieve mails of a user', async () => {
-        const mails = await simpleMail.retrieveMail();
-
-        expect(mails.length).toBeGreaterThan(0);
-    });
-
-    test('send a mail', async () => {
-        const result = await simpleMail.sendMail(
-            'douglas.hofstadter@simplemail.com',
-            'Hello',
-            'does it works ?');
-
-        expect(result.status).toEqual(200);
-
-        const mails = await simpleMail.retrieveMail();
-
-        expect(mails).toContainEqual({
-            from:'douglas.hofstadter@simplemail.com',
-            to: 'douglas.hofstadter@simplemail.com',
-            subject:'Hello',
-            body:'does it works ?'
-        })
+    test('discover how simple mail api works', () => {
 
     });
 });
