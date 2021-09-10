cf50e6f4945b025b67a7a5df323c9ad3eedb05e4
mathieu cans <mathieucans@gmail.com>
2021-09-10 14:04:23 +0200

    Add some tip to try to be faster

--- simplemail-client/typescript/src/simplemail.integration.spec.ts
+++ simplemail-client/typescript/src/simplemail.integration.spec.ts
@@ -1,4 +1,6 @@
 describe('Simple mail api integration tests', () => {
+    // interact with server http://localhost:8080/:userId/mails
+    // user id 1
     test('discover how simple mail api works', () => {
 
     });
