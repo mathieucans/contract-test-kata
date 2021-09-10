3f0db2ffe6fd1224bf1e9407bd5c4117b2db68f5
mathieu cans <mathieucans@gmail.com>
2021-09-10 15:58:18 +0200

    Add tools to tes in local server

--- simplemail-client/typescript/src/Simplemail.integration.spec.ts
+++ simplemail-client/typescript/src/Simplemail.integration.spec.ts
@@ -1,6 +1,6 @@
+// interact with server http://localhost:8080/:userId/mails
+// user id 1
 describe('Simple mail api integration tests', () => {
-    // interact with server http://localhost:8080/:userId/mails
-    // user id 1
     test('discover how simple mail api read mails works', () => {
 
     });
