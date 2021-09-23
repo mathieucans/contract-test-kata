2a9043235509b5ecde2059e36396684c981f0867
mathieu cans <mathieucans@gmail.com>
2021-09-23 15:38:40 +0200

    Rename mail with message

---
+++ simplemail-client/typescript/src/SimpleMessage.integration.spec.ts
@@ -0,0 +1,9 @@
+// interact with server
+// http://localhost:8080/:userId/messages
+// and user id 1
+describe('Simple message api integration tests', () => {
+    test('list received messages', () => {
+
+    });
+});
+
--- simplemail-client/typescript/src/Simplemail.integration.spec.ts
+++
@@ -1,8 +0,0 @@
-// interact with server http://localhost:8080/:userId/mails
-// user id 1
-describe('Simple mail api integration tests', () => {
-    test('discover how simple mail api read mails works', () => {
-
-    });
-});
-
