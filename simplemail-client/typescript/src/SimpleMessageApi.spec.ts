ad756c6e234e2ce6892eaee27817d883cd839b1f
mathieu cans <mathieucans@gmail.com>
2021-09-24 10:18:06 +0200

    Add some skeleton to be faster in presentation

--- simplemail-client/typescript/src/SimpleMessageApi.spec.ts
+++ simplemail-client/typescript/src/SimpleMessageApi.spec.ts
@@ -1,13 +1,20 @@
 import {LocalHttpServer} from "./LocalHttpServer";
+import {SimpleMessageSdk} from "./SimpleMessageSdk";
 
 describe('SimpleMailApi', () => {
     let server: any;
+    let simpleMessageSdk: SimpleMessageSdk;
     beforeEach(async () => {
         server = await new LocalHttpServer();
         await server.start(1234);
+        simpleMessageSdk = new SimpleMessageSdk('http://localhost:1234');
     });
 
     afterEach(async () => {
         await server.stop();
+    });
+
+    test('list received messages', async () => {
+
     });
 });
