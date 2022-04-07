eb0937fa9efa0c2113e19b4361276d178ad38b84
mathieu cans <mathieucans@gmail.com>
2022-04-07 20:36:21 +0200

    Rename SimpleMessage.contract.spec.ts

--- simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
+++ simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
@@ -1,7 +1,7 @@
 // interact with server
 // http://localhost:8080/:userId/messages
 // and user id 1
-describe('Simple message api integration tests', () => {
+describe('Simple message api contract tests', () => {
     test('list received messages', () => {
 
     });
