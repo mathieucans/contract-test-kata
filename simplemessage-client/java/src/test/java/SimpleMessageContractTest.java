af42d00976871aa6048fe042fd6321dc84af85a4
mathieu <mathieucans@gmail.com>
2022-11-11 19:10:39 +0100

    Add java 11 setup

---
+++ simplemessage-client/java/src/test/java/SimpleMessageContractTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+
+public class SimpleMessageContractTest {
+    @Test
+    void listReceivedMessages() {
+        fail("interact with server http://localhost:8080/:userId/messages and user id 1");
+    }
+}
