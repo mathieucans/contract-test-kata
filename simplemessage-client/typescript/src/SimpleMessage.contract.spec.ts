ecaab936632f7b835eb161d970efa30a2563bc20
mathieu <mathieucans@gmail.com>
2022-11-11 19:22:38 +0100

    Update project name

--- simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
+++ simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
@@ -1,9 +1,11 @@
 // interact with server
 // http://localhost:8080/:userId/messages
 // and user id 1
-describe('Simple message api contract tests', () => {
-    test('list received messages', () => {
+import request from "superagent";
 
+describe('Simple message api contract tests', () => {
+    test('list received messages', async () => {
+        await request.get(`http://localhost:8080/1/messages`)
     });
 });
 
