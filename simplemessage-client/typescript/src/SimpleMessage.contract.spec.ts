b65830163a9367a5eb9965e1b0e65c5e090ca251
mathieu <mathieucans@gmail.com>
2022-11-14 17:10:28 +0100

    Cleanup typescript setup

--- simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
+++ simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
@@ -1,11 +1,9 @@
 // interact with server
 // http://localhost:8080/:userId/messages
 // and user id 1
-import request from "superagent";
 
 describe('Simple message api contract tests', () => {
     test('list received messages', async () => {
-        await request.get(`http://localhost:8080/1/messages`)
     });
 });
 
