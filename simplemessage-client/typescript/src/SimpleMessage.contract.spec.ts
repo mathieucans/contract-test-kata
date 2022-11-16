44786a6a68383ba61c926df9774964a15c96c31b
mathieu <mathieucans@gmail.com>
2022-11-16 21:07:04 +0100

    A first red test that teach how error status are managed with the chosen library

--- simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
+++ simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
@@ -2,8 +2,12 @@
 // http://localhost:8080/:userId/messages
 // and user id 1
 
+import request from "superagent";
+
 describe('Simple message api contract tests', () => {
     test('list received messages', async () => {
+        const response = await request.get('http://localhost:8080/:userId/messages');
+        expect(response.status).toEqual(200);
     });
 });
 
