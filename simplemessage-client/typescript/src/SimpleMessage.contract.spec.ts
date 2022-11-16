a3b7cd0728fd738ffbe4a53790e865bdc1976c45
mathieu <mathieucans@gmail.com>
2022-11-16 21:09:37 +0100

    First green phase with list received messages

--- simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
+++ simplemessage-client/typescript/src/SimpleMessage.contract.spec.ts
@@ -7,7 +7,16 @@ import request from "superagent";
 describe('Simple message api contract tests', () => {
     test('list received messages', async () => {
         const response = await request.get('http://localhost:8080/1/messages');
-        expect(response.body).toEqual({});
+        expect(response.body).toEqual({
+            "messages": [
+                {
+                    "from": "someone",
+                    "id": "1",
+                    "message": "hello doug from LA!",
+                    "to": "douglas.hofstadter"
+                }
+            ]
+        });
     });
 });
 
